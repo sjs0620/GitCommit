497d9b896a8efedce5d60d652586cdbf6e10f8a2
신재순 <sjslsm@gmail.com>
2018-11-02 15:36:44 +0900

    Initial commit

---
+++ app/src/main/java/com/sjs/edu/gitcommit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sjs.edu.gitcommit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
